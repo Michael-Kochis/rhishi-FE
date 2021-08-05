f3eb5cf102742f5682a6fc9a20f5b802dd7b0ac2
Michael-Kochis <rhishisikk@gmail.com>
2021-08-05 17:08:42 -0500

    Now pulls persona from database

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,7 +1,22 @@
+import React, { useEffect, useState } from 'react';
+import axiosWithAuth from '../utils/axiosWithAuth';
+
 function Dashboard(props) {
+    const [persona, setPersona] = useState([]);
+
+    useEffect(() => {
+        axiosWithAuth().get("/persona")
+            .then((resp) => {
+                setPersona(resp.data)
+            })
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+      }, []);  
+
     return (
         <div>
-            This is the dashboard
+            {persona && persona.map((star) => {
+                return <p>{star.personaName}</p>
+            })}
         </div>
     )
 }
